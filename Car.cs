c8644d41a60452bedcee86cca5136ce591bfb11e
Thanh Dat <datmathoingu@gmail.com>
2020-09-21 22:28:42 +0700

    Initial commit

---
+++ Car.cs
@@ -0,0 +1,16 @@
+﻿namespace StaticProperty
+{
+    public class Car
+    {
+
+        private string name;
+        private string engine;
+        public static int numberOfCars;
+        public Car(string name, string engine)
+        {
+            this.name = name;
+            this.engine = engine;
+            numberOfCars++;
+        }
+    }
+}
